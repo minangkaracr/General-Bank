aadbc2b69b5cba29507ab7c38184bc66edf93d2e
Minangkara Rengga <49264068+minangkaracr@users.noreply.github.com>
2023-12-20 12:07:28 +0700

    Update auth_logic.py

--- general-bank/logic/auth_logic.py
+++ general-bank/logic/auth_logic.py
@@ -7,6 +7,7 @@ import pytz
 auth_blueprint = Blueprint('auth', __name__)
 now = datetime.now()
 tmzone = pytz.timezone('Asia/Jakarta')
+code = tmzone.localize(now).strftime("%Y%m%d")
 date_now = tmzone.localize(now).strftime("%Y-%m-%d %H:%M:%S")
 
 def register(request):
@@ -20,22 +21,39 @@ def register(request):
     dob = data.get('dob')
     cust_type = data.get('cust_type')
     
-    cursor.execute("select username, email_address from customer c where username = %s or email_address = %s", (username, email,))
+    
+    cursor.execute("select username, email_address from customer c where username = %s", (username,))
     user = cursor.fetchone()
+    cursor.execute("select username, email_address from customer c where email_address = %s", (email,))
+    mail = cursor.fetchone()
+    
+    cursor.execute("select count(id) from customer c where date_trunc('day', created_at) = current_date")
+    curr_num = cursor.fetchone()[0] + 1
+    
+    if curr_num < 10:
+        customer_id = '0000' + code + '00' + str(curr_num)
+    elif curr_num < 100:
+        customer_id = '0000' + code + '0' + str(curr_num)
+    elif curr_num < 1000:
+        customer_id = '0000' + code + str(curr_num)
     
     if user:
         return jsonify({
-            'message':'User already exist.'
+            'message':'Username already exist.'
+        })
+    if mail:
+        return jsonify({
+            'message':'Email already exist.'
         })
     
     # hashing password using bcrypt
     hashed_password = bcrypt.hashpw(password, bcrypt.gensalt())
     
     try:
-        cursor.execute("INSERT INTO public.customer (id, username, password, email_address, first_name, last_name, address, dob, created_at) VALUES(nextval('customer_id_seq'::regclass), %s, %s, %s, %s, %s, %s, %s, %s)",(username, hashed_password.decode('utf-8'),email, first_name, last_name, address, dob, date_now))
+        cursor.execute("INSERT INTO public.customer (id, username, password, email_address, first_name, last_name, address, dob, created_at, customer_id) VALUES(nextval('customer_id_seq'::regclass), %s, %s, %s, %s, %s, %s, %s, %s, %s)",(username, hashed_password.decode('utf-8'),email, first_name, last_name, address, dob, date_now, customer_id,))
         conn.commit()
         
-        cursor.execute("INSERT INTO public.customer_type (id, customer_type, last_update) VALUES(nextval('customer_type_id_seq'::regclass), %s, %s)",(cust_type, date_now,))
+        cursor.execute("INSERT INTO public.customer_type (id, customer_type, last_update, customer_id) VALUES(nextval('customer_type_id_seq'::regclass), %s, %s, %s)",(cust_type, date_now, customer_id,))
         conn.commit()
         
         return jsonify({
